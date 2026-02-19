d7d713c3256d8544e7769b6fe38ccd753e715704
Sub1111 <vahrushev16092009@gmail.com>
2026-02-19 20:00:18 +0300

    upside down raymarching without lighting

--- VecFunc.h
+++ VecFunc.h
@@ -1,4 +1,38 @@
 #include "Vec2.h"
 #include "Vec3.h"
+#include <math.h>
 
+using namespace std;
 
+#define MAX_STEPS 100
+#define MAX_DIST 100.0f
+#define SURF_DIST 0.01f
+
+float length(vec2 const& v) { return sqrt(v.x * v.x + v.y * v.y);}
+float length(vec3 const& v) { return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);}
+
+vec3 normalize(vec3 v) { return v / length(v);}
+
+float sdSphere(vec3 p, vec3 const& pos, float r){ return length(p - pos) - r;}
+float sdPlane(vec3 p, float y){ return p.y - y;}
+
+float GetDist(vec3 const& p){
+    float dS = sdSphere(p, vec3(0, 2, 4), 1);
+    float dP = sdPlane(p, 0);
+
+    float d = min(dS, dP);
+    return d;
+}
+
+float RayMarch(vec3 ro, vec3 rd){
+    float dO = 0.0f;
+
+    for(int i = 0; i<MAX_STEPS; i++){
+        vec3 p = ro + rd * dO;
+        float dS = GetDist(p);
+        dO += dS;
+        if(dO > MAX_DIST || dS < SURF_DIST) break;
+    }
+
+    return dO;
+}
--- main.cpp
+++ main.cpp
@@ -25,25 +25,34 @@ int main(){
     int k = 8;
     int width = 16 * k;
     int height = 9 * k;
-	vec2 resolution = vec2(float(width), float(height));
 
     float pixelRatio = 8.0f/16.0f;
     width = floor(width / pixelRatio);
     float screenRatio = float(width) / float(height);
+	vec2 resolution = vec2(float(width), float(height));
 
 	wstring screen(width * height, L' ');
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     DWORD written;
+	wstring gradient = L" .:!/r(l1Z4H9W8$@";
+	wchar_t finalPixel = L' ';
 
     SetWindow(width, height);
     for (int t = 0; t < 100000; t++){
 		for (int x = 0; x < width; x++){
 			for (int y = 0; y < height; y++){
-				wchar_t finalPixel = L' ';
-				vec2 uv = vec2((x - width / 2) / float(width), (y - height / 2) / float(height));
+				vec2 uv = vec2(x - width / 2, y - height / 2) / resolution;
 				uv.x = uv.x * pixelRatio * screenRatio;
-				if (uv.x * uv.x + uv.y * uv.y < 0.5f)
-					finalPixel = L'#';
+
+				finalPixel = L' ';
+
+				vec3 ro = vec3(0, 1, 0);
+				vec3 rd = normalize(vec3(uv));
+
+				float d = RayMarch(ro, rd);
+				
+				int gradientInd = round(d/6.0f * 17);
+				finalPixel = gradient[min(16, gradientInd)];
 				
 				screen[y * width + x] = finalPixel;
 			}
